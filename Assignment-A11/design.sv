b18169c276265fb1a214f1b7b7bef74443adc3bd
Sim Jia Jun <118655979+JJ07-ops@users.noreply.github.com>
2024-08-07 16:44:05 +0800

    Create design.sv

---
+++ Assignment-A11/design.sv
@@ -0,0 +1,21 @@
+//design code given in the question
+module dff (dff_if vif);
+  
+  always@(posedge vif.clk)
+    begin
+      if(vif.rst == 1'b1)
+        vif.dout <= 1'b0;
+      else
+         vif.dout <= vif.din;
+    end
+  
+endmodule
+ 
+ 
+interface dff_if;
+  logic clk;
+  logic rst;
+  logic din;
+  logic dout;
+  
+endinterface
